f835d13f0639a29bc21e54bbd4f594fba6afe861
thisisjose <josecinuwu@gmail.com>
2025-06-25 22:16:24 -0700

    primer commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Persona
+{
+    private string nombre;
+    private int edad;
+
+    public Persona(string nombre, int edad)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void Saludar()
+    {
+        Console.WriteLine($"Hola, mi nombre es {nombre} y tengo {edad} años.");
+    }
+
+    public void CumplirAnios()
+    {
+        edad++;
+        Console.WriteLine($"{nombre} ahora tiene {edad} años.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Persona persona1 = new Persona("Ana", 20);
+        persona1.Saludar();
+        persona1.CumplirAnios();
+    }
+}
